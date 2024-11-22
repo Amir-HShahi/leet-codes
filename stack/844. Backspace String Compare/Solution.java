786c70087925576680ced84299804e0244cbe595
Amir-HShahi <amir13831107h@gmail.com>
2024-11-22 19:49:57 +0330

    submit Backspace String Compare

--- stack/844. Backspace String Compare/Solution.java
+++ stack/844. Backspace String Compare/Solution.java
@@ -1,23 +1,25 @@
 import java.util.Stack;
 
 class Solution {
-    public boolean backspaceCompare(String s, String t) {
+    public static boolean backspaceCompare(String s, String t) {
         Stack<Character> firstStack = new Stack<Character>();
         Stack<Character> secondStack = new Stack<Character>();
         for (Character character : s.toCharArray()) {
             if (!firstStack.isEmpty() && character.equals('#')) {
                 firstStack.pop();
-            } else {
+            } else if (!character.equals('#')) {
                 firstStack.push(character);
             }
         }
+        System.out.println(firstStack);
         for (Character character : t.toCharArray()) {
             if (!secondStack.isEmpty() && character.equals('#')) {
                 secondStack.pop();
-            } else {
+            } else if (!character.equals('#')){
                 secondStack.push(character);
             }
         }
+        System.out.println(secondStack);
         while (!firstStack.isEmpty() && !secondStack.isEmpty()) {
             if (firstStack.pop() != secondStack.pop()) {
                 return false;
