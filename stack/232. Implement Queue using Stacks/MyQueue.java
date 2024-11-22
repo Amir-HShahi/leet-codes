e8a4f78f5338a8fc3deccc128511fc7d366684d1
Amir-HShahi <amir13831107h@gmail.com>
2024-11-22 11:42:20 +0330

    submit MyQueue

--- stack/232. Implement Queue using Stacks/MyQueue.java
+++ stack/232. Implement Queue using Stacks/MyQueue.java
@@ -1,21 +1,36 @@
+import java.util.Stack;
+
 class MyQueue {
-    public MyQueue() {
-        
-    }
+    Stack<Integer> firstStack = new Stack<Integer>();
+    Stack<Integer> secondStack = new Stack<Integer>();
     
     public void push(int x) {
-        
+        firstStack.push(x);
     }
     
     public int pop() {
-        
+        while (!firstStack.empty()) {
+            secondStack.push(firstStack.pop());
+        }
+        int pop = secondStack.pop();
+        while (!secondStack.empty()) {
+            firstStack.push(secondStack.pop());
+        }
+        return pop;
     }
     
     public int peek() {
-        
+        while (!firstStack.empty()) {
+            secondStack.push(firstStack.pop());
+        }
+        int peek = secondStack.peek();
+        while (!secondStack.empty()) {
+            firstStack.push(secondStack.pop());
+        }
+        return peek;
     }
     
     public boolean empty() {
-        
+        return firstStack.empty() && secondStack.empty();
     }
 }
