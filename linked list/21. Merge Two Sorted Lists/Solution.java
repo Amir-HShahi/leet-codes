5e9657ef228b07b64cf604d3d610c3f2084075b0
Amir-HShahi <amir13831107h@gmail.com>
2024-11-23 13:28:45 +0330

    submit merge linked list

--- linked list/21. Merge Two Sorted Lists/Solution.java
+++ linked list/21. Merge Two Sorted Lists/Solution.java
@@ -1,29 +1,18 @@
-
-class ListNode {
-     int val;
-     ListNode next;
-     ListNode() {}
-     ListNode(int val) { this.val = val; }
-     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
-
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode mergedList = new ListNode();
-        mergedList.val = list1.val > list2.val ? list2.val : list1.val;
         ListNode key = mergedList;
-        while (list1 != null || list2 != null) {
-            ListNode newNode = new ListNode();
-            if ((list2 != null && list1 == null) || list1.val > list2.val) {
-                newNode.val = list2.val;
-                list2 = list2.next;
-            } else {
-                newNode.val = list1.val;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                key.next = list1;
                 list1 = list1.next;
+            } else {
+                key.next = list2;
+                list2 = list2.next;
             }
-            key.next = newNode;
-            key = newNode;
+            key = key.next;
         }
-        return mergedList;
+        key.next = list1 == null ? list2 : list1;
+        return mergedList.next;
     }
 }
