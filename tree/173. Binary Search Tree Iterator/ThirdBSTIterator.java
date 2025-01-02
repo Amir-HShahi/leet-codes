966428c649c6c33215df31a2f60eabe1ec710edd
Amir-HShahi <amir13831107h@gmail.com>
2025-01-02 16:58:23 +0330

    ThirdBSTIterator

---
+++ tree/173. Binary Search Tree Iterator/ThirdBSTIterator.java
@@ -0,0 +1,49 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class ThirdBSTIterator {
+    private int index = 0;
+    private ArrayList<TreeNode> inorderNodes = new ArrayList<TreeNode>();
+    private int size;
+
+    public ThirdBSTIterator(TreeNode root) {
+        inorderInit(root);
+        this.size = inorderNodes.size();
+    }
+
+    public int next() {
+        return inorderNodes.get(index++).val;
+    }
+
+    public boolean hasNext() {
+        return index < size;
+    }
+
+    private void inorderInit(TreeNode root) {
+        if (root == null)
+            return;
+
+        inorderInit(root.left);
+        inorderNodes.add(root);
+        inorderInit(root.right);
+    }
+}
