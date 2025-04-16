3282e444f0092f3444eb2df7c8f90bbc3d9f4227
Amir-HShahi <amir13831107h@gmail.com>
2025-04-16 12:10:02 +0330

    pascal's triangle

---
+++ dynamic programming/118. Pascal's Triangle/Solution.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> rows = new ArrayList<List<Integer>>();
+        for (int length = 1; length <= numRows; length++) {
+            Integer[] row = new Integer[length];
+            row[0] = 1;
+            row[length - 1] = 1;
+            for (int i = 1; i < row.length - 1; i++) {
+                List<Integer> prevRow = rows.get(length - 2);
+                row[i] = prevRow.get(i) + prevRow.get(i - 1);
+            }
+            rows.add(Arrays.asList(row));
+        }
+        return rows;
+    }
+}
