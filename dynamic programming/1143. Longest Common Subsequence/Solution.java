93cde05fcbf3acabada75c92f5c3ae6a6f3f5943
Amir-HShahi <amir13831107h@gmail.com>
2025-05-11 18:46:37 +0330

    LCS TopDown

--- dynamic programming/1143. Longest Common Subsequence/Solution.java
+++ dynamic programming/1143. Longest Common Subsequence/Solution.java
@@ -1,18 +1,30 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        return longestCommonSubsequence(text1.toCharArray(), text2.toCharArray(), text1.length() - 1,
-                text2.length() - 1);
+        Integer[][] dp = new Integer[text1.length()][text2.length()];
+
+        return longestCommonSubsequence(
+                text1.toCharArray(),
+                text2.toCharArray(),
+                text1.length() - 1,
+                text2.length() - 1, dp);
     }
 
-    private int longestCommonSubsequence(char[] text1, char[] text2, int i, int j) {
-        if (i == -1 || j == -1)
+    private int longestCommonSubsequence(char[] text1, char[] text2, int i, int j, Integer[][] dp) {
+        if (i < 0 || j < 0)
             return 0;
 
-        if (text1[i] == text2[j])
-            return 1 + longestCommonSubsequence(text1, text2, --i, --j);
+        if (dp[i][j] != null)
+            return dp[i][j];
 
-        return Math.max(
-                longestCommonSubsequence(text1, text2, i - 1, j),
-                longestCommonSubsequence(text1, text2, i, j - 1));
+        int cost;
+        if (text1[i] == text2[j]) {
+            cost = 1 + longestCommonSubsequence(text1, text2, i - 1, j - 1, dp);
+        } else {
+            cost = Math.max(
+                    longestCommonSubsequence(text1, text2, i - 1, j, dp),
+                    longestCommonSubsequence(text1, text2, i, j - 1, dp));
+        }
+        dp[i][j] = cost;
+        return cost;
     }
 }
