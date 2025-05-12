0a49c88542155b04b5cd7dd744120c74e8c285c2
Amir-HShahi <amir13831107h@gmail.com>
2025-05-12 14:15:05 +0330

    LCS BottomUp

--- dynamic programming/1143. Longest Common Subsequence/Solution.java
+++ dynamic programming/1143. Longest Common Subsequence/Solution.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int longestCommonSubsequence(String text1, String text2) {
+    public int longestCommonSubsequenceTD(String text1, String text2) {
         Integer[][] dp = new Integer[text1.length()][text2.length()];
 
         return longestCommonSubsequence(
@@ -7,6 +7,22 @@ class Solution {
                 text2.toCharArray(),
                 text1.length() - 1,
                 text2.length() - 1, dp);
+    }
+
+    public int longestCommonSubsequenceBU(String q, String p) {
+        int n = q.length();
+        int m = p.length();
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (q.charAt(i - 1) == p.charAt(j - 1))
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+            }
+        }
+        return dp[n][m];
     }
 
     private int longestCommonSubsequence(char[] text1, char[] text2, int i, int j, Integer[][] dp) {
