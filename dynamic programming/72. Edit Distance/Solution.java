3ed8c3d19111c84acd6061b572735d5395747e11
Amir-HShahi <amir13831107h@gmail.com>
2025-05-15 12:10:29 +0330

    Edit Distsance TopDown

--- dynamic programming/72. Edit Distance/Solution.java
+++ dynamic programming/72. Edit Distance/Solution.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int minDistance(String word1, String word2) {
+    public int minDistanceBU(String word1, String word2) {
         int length1 = word1.length();
         int length2 = word2.length();
         int[][] dp = new int[length1 + 1][length2 + 1];
@@ -23,5 +23,42 @@ class Solution {
             }
         }
         return dp[length1][length2];
+    }
+
+    public int minDistanceTD(String word1, String word2) {
+        int length1 = word1.length();
+        int length2 = word2.length();
+        if (length1 == 0) {
+            return length2;
+        }
+
+        if (length2 == 0) {
+            return length1;
+        }
+
+        int[][] dp = new int[length1 + 1][length2 + 1];
+        return minDistance(word1, word2, length1, length2, dp);
+    }
+
+    public int minDistance(String word1, String word2, int i, int j, int[][] dp) {
+        if (i == 0) {
+            return dp[i][j] = j;
+        }
+        if (j == 0) {
+            return dp[i][j] = i;
+        }
+        if (dp[i][j] > 0) {
+            return dp[i][j];
+        }
+        if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+            return dp[i][j] = minDistance(word1, word2, i - 1, j - 1, dp);
+        } else {
+            return dp[i][j] = Math.min(
+                    minDistance(word1, word2, i - 1, j - 1, dp),
+                    Math.min(
+                            minDistance(word1, word2, i, j - 1, dp),
+                            minDistance(word1, word2, i - 1, j, dp)))
+                    + 1;
+        }
     }
 }
