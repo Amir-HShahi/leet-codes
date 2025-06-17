14979d17b6a74fd4bb2f3dd79a09a16626b5211b
Amir-HShahi <amir13831107h@gmail.com>
2025-06-17 20:02:05 +0330

    LCA

---
+++ tree/236. Lowest Common Ancestor of a Binary Tree/Solution.java
@@ -0,0 +1,22 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        int val = 1;
+        if (val > p.val && val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (val < p.val && val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            return root;
+        }
+    }
+}
