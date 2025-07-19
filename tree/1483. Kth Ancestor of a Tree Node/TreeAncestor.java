b8fdcd360d34223059a28b27f1f40e27ee4bfed5
Amir-HShahi <amir13831107h@gmail.com>
2025-07-19 15:31:48 +0330

    Add: Kth ancestor of a tree node

--- tree/1483. Kth Ancestor of a Tree Node/TreeAncestor.java
+++ tree/1483. Kth Ancestor of a Tree Node/TreeAncestor.java
@@ -6,20 +6,19 @@ class TreeAncestor {
         logN = (int) (Math.log(n) / Math.log(2)) + 1;
         up = new int[n][logN];
 
-        for (int i = 0; i < logN; i++)
-            up[0][i] = -1;
-
         for (int i = 0; i < n; i++)
             up[i][0] = parent[i];
 
-        for (int i = 1; i < n; i++)
-            for (int j = 1; j < logN; j++)
+        for (int j = 1; j < logN; j++)
+            for (int i = 0; i < n; i++)
                 if (up[i][j - 1] != -1)
                     up[i][j] = up[up[i][j - 1]][j - 1];
+                else
+                    up[i][j] = -1;
     }
 
     public int getKthAncestor(int node, int k) {
-        for (int i = 0; i < this.logN; i++) {
+        for (int i = 0; i < logN; i++) {
             if (node == -1)
                 return -1;
 
